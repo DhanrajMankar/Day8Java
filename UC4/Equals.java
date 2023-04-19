60e6b63ac94ecd39034a2ce27845e68106e7c9ea
Dhanraj <dhanrajmankar30@gmail.com>
2023-04-19 10:41:26 +0530

    This is UC4

---
+++ UC4/Equals.java
@@ -0,0 +1,17 @@
+package Day8Java;
+
+public class Equals {
+
+	double LineLength;
+	public boolean equal(double l1,double l2)
+	{
+		boolean c =false;
+		if(l1==l2)
+		{
+		 c = true;
+		}
+		
+		return c;
+	}
+	
+}
---
+++ UC4/GeometryComparison.java
@@ -0,0 +1,110 @@
+package Day8Java;
+import java.util.*;
+
+public class GeometryComparison extends LengthCal{
+
+	public static void main(String[] args) {
+		/*
+		 * Creating an object class 
+		 * */
+		Scanner sc = new Scanner(System.in);
+		Co_Ordinates line1 = new Co_Ordinates();
+		Co_Ordinates line2 = new Co_Ordinates();
+		GeometryComparison ob1=new GeometryComparison();
+		
+		
+		System.out.println("For line1 pls enter the values");
+		System.out.println("Pls Enter value of x1 : ");
+		int x1 = sc.nextInt();
+		// Setting x1 value
+		line1.setX1(x1);
+		
+		System.out.println("Pls Enter value of y1 : ");
+		int y1 = sc.nextInt();
+		// Setting y1 value
+		line1.setY1(y1);
+		
+		System.out.println("Pls Enter value of x2 : ");
+		int x2 = sc.nextInt();
+		// Setting x2 value
+		line1.setX2(x2);
+		
+		System.out.println("Pls Enter value of y2 : ");
+		int y2 = sc.nextInt();
+		// Setting y2 value
+		line1.setY2(y2);
+		
+		/*Calling a LengthCal method and passing vales using get method*/
+		double Line1length =ob1.LengthCal(line1.getX1(),line1.getX2(),line1.getY1(),line1.getY2());
+        
+		System.out.println("For line2 pls enter the values");
+		
+		
+		System.out.println("Pls Enter value of x1 : ");
+		 x1 = sc.nextInt();
+		// Setting x1 value
+		line2.setX1(x1);
+		
+		System.out.println("Pls Enter value of y1 : ");
+		y1 = sc.nextInt();
+		// Setting y1 value
+		line2.setY1(y1);
+		
+		System.out.println("Pls Enter value of x2 : ");
+		x2 = sc.nextInt();
+		// Setting x2 value
+		line2.setX2(x2);
+		
+		System.out.println("Pls Enter value of y2 : ");
+		y2 = sc.nextInt();
+		// Setting y2 value
+		line2.setY2(y2);
+		
+		/*Calling a LengthCal method and passing vales using get method*/
+		double Line2length =ob1.LengthCal(line2.getX1(),line2.getX2(),line2.getY1(),line2.getY2());
+		
+		if(Line1length==Line2length)
+		{
+			System.out.println("Both lines are of same length");	
+		}
+		else if(Line1length>Line2length)
+		{
+			System.out.println("Length of Line1 is more than Line2");		
+		}
+		else if(Line1length<Line2length)
+		{
+			System.out.println("Length of Line1 is less than Line2");		
+		}
+		
+		
+		String Line1=Double.toString(Line1length);	
+		String Line2=Double.toString(Line2length);
+	
+		System.out.println("This is a Result of 'compareTo' Method : "+Line1.compareTo(Line2));
+		if(Line1.compareTo(Line2)==1)
+		{
+			System.out.println("Both lines are of same length");
+		}
+		System.out.println();
+		
+        // Creating object of Equals class		
+		Equals eql=new Equals();
+		
+		// Calling equal method of Equals class and passing length of lines as parameter 
+		// It returns true if length is same and if length is not same it return false
+		System.out.println("This is a Result of 'equal' Method : "+eql.equal(Line1length, Line2length));
+		if(eql.equal(Line1length, Line2length))
+		{
+			System.out.println("Both lines are of same length");
+		}
+		
+	}
+
+	@Override
+ 
+	double LengthCal(int x1,int x2,int y1,int y2) {
+		double l= Math.sqrt(Math.pow((x2-x1), 2)+Math.pow((y2-y1),2));
+		return l;
+	}
+
+}
